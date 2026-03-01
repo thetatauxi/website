afc6b682ca81d63f58c2493ff5cb1b0df73e691d
andrew <me@andreww.co>
2026-03-01 16:12:07 -0600

    fix next-mdx v5 vulnerability

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withMDX = createMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["next-mdx-remote"],
   eslint: {
     ignoreDuringBuilds: true,
   },
